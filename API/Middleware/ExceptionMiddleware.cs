1945ab7aee7496f1f9849520b33985a4c97e150e
Jesus <jesus555jimenez@gmail.com>
2022-04-05 13:39:38 +0200

    test1 publicacion

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -41,9 +41,12 @@ namespace API.Middleware
                 ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
                 : new ApiException(context.Response.StatusCode, "Internal Server Error");
 
+                //var response = new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString());
+
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
 
+                
                 await context.Response.WriteAsync(json);
             }
         }
